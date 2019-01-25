22863d090047ac427d676153b4585b1b3f21bc59
Nawab69 <44573643+nawab69@users.noreply.github.com>
2019-01-25 14:55:42 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+/* Include header file */
+
+include ('include/header.php');
+
+/* Include navigation bar file */
+
+include ('include/nav.php');
+?>
+	<div class="container">
+ 									<!-- Content here --> 
+ 					<br>
+ 					<div class="row"> 
+ 							<div class="col-sm">
+ 							<a href="check.php">
+ 							 <div class="card text-white bg-info mb-3">
+ 							  <div class="card-body"> <center>
+ 							  <h5 class="card-title"> Check Current Withdraw Route</h5>
+ 	</center>
+ 							  </div></div> </a>
+ 							  
+ 							  </div>
+ 							  
+ 							   <div class="col-sm"> <a href="change.php">
+ 							 <div class="card text-white bg-success mb-3">
+ 							  <div class="card-body"> <center>
+ 							  <h5 class="card-title"> Change Withdraw Route</h5>
+ 	</center>
+ 							  </div></div> </a>
+ 							  </div> 
+ 							   <div class="col-sm"> <a href="check.php">
+ 							 <div class="card text-white bg-danger mb-3">
+ 							  <div class="card-body"> <center>
+ 							  <h5 class="card-title"> Remove Withdraw Route</h5>
+ 	</center>
+ 							  </div></div> </a>
+ 							   </div>
+ 							   
+ 							   
+ 							    </div>
+ 									
+ 									
+ 									
+ 									
+
+			</div>
+
+<?php
+ include ('include/footer.php');
+?>
+
+
+
+
+
---
+++ nav.php
@@ -0,0 +1,28 @@
+<body>
+			<nav class="navbar navbar-dark bg-dark"> 
+					<a class="navbar-brand" href="#"> Withdraw Route </a>
+
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> 
+										<span class="navbar-toggler-icon">
+										</span> 
+          </button> 
+<div class="collapse navbar-collapse" id="navbarNavDropdown"> 
+								<ul class="navbar-nav"> 
+										<li class="nav-item active">
+										 <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span>
+										 </a> 
+										</li> 
+										<li class="nav-item">
+										 <a class="nav-link" href="check.php">Check Withdraw Route</a>
+									 </li> 
+									 <li class="nav-item">
+									  <a class="nav-link" href="change.php">Change Withdraw Route</a> 
+									  </li>
+                        <li class="nav-item">
+									  <a class="nav-link" href="remove.php">Remove Withdraw Route</a> 
+									  </li>
+							   </ul>
+        </div>
+<!-- Navbar content --> 
+				
+			</nav> 
