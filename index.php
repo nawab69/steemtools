45b4adee0de9ca40318656566d5f64581759d0b1
Nawab69 <44573643+nawab69@users.noreply.github.com>
2019-01-31 23:16:42 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,49 @@ include ('include/nav.php');
 ?>
 	<div class="container">
  									<!-- Content here --> 
- 					<br>
+ 					<br>		
+                                                  <!-- Steemtools Api Card -->
+		
+	<div class="card"> <h5 class="card-header">STEEM TOOLS API</h5> <div class="card-body"> 
  					<div class="row"> 
  							<div class="col-sm">
- 							<a href="check.php">
+ 							<a href="http://api.steemtools.cf/get_blog_count?url=">
+ 							 <div class="card text-white bg-info mb-3">
+ 							  <div class="card-body"> <center>
+ 							  <h5 class="card-title">get_blog_count</h5>
+ 	</center>
+ 							  </div></div> </a>
+ 							  
+ 							  </div>
+ 							  
+ 							  
+ 							   </div>
+</div>
+ 							   
+ 							    </div>
+
+
+ 										<div class="card"> <h5 class="card-header">BLOG</h5> <div class="card-body"> 
+ 					<div class="row"> 
+ 							<div class="col-sm">
+ 							<a href="blog_count">
+ 							 <div class="card text-white bg-warning mb-3">
+ 							  <div class="card-body"> <center>
+ 							  <h5 class="card-title">Blog Counter</h5>
+ 	</center>
+ 							  </div></div> </a>
+ 							  
+ 							  </div>
+ 							  
+ 							  
+ 							   </div>
+</div>
+ 							   
+ 							    </div>
+ 										<div class="card"> <h5 class="card-header">Withdraw Routes</h5> <div class="card-body"> 
+ 					<div class="row"> 
+ 							<div class="col-sm">
+ 							<a href="check">
  							 <div class="card text-white bg-info mb-3">
  							  <div class="card-body"> <center>
  							  <h5 class="card-title"> Check Current Withdraw Route</h5>
@@ -22,25 +61,24 @@ include ('include/nav.php');
  							  
  							  </div>
  							  
- 							   <div class="col-sm"> <a href="change.php">
+ 							   <div class="col-sm"> <a href="change">
  							 <div class="card text-white bg-success mb-3">
  							  <div class="card-body"> <center>
  							  <h5 class="card-title"> Change Withdraw Route</h5>
  	</center>
  							  </div></div> </a>
  							  </div> 
- 							   <div class="col-sm"> <a href="check.php">
+ 							   <div class="col-sm"> <a href="remove">
  							 <div class="card text-white bg-danger mb-3">
  							  <div class="card-body"> <center>
  							  <h5 class="card-title"> Remove Withdraw Route</h5>
  	</center>
  							  </div></div> </a>
  							   </div>
- 							   
+ 							   </div>
+</div>
  							   
  							    </div>
- 									
- 									
  									
  									
 
