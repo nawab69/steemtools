fdd1827c13815aece42c12b6fd67bf77ecad3938
Nawab69 <44573643+nawab69@users.noreply.github.com>
2019-03-08 12:24:34 +0600

    Create functions.php
    Add GetData() functions for getting data and post data in json

---
+++ API/functions.php
@@ -0,0 +1,37 @@
+
+<?php
+
+// API //
+
+$api="https://api.steemit.com";
+
+// Curl Method //
+
+function GetData($api,$pdata){
+
+$ch = curl_init($api);
+     // steemit api
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $pdata);
+  //post data
+curl_setopt($ch, CURLOPT_HTTPHEADER, false);
+ // http header request
+$data = curl_exec($ch);
+     //get data from steemit API
+curl_close($ch);
+  
+   //close curl
+
+$arr = json_decode($data,true);
+      //decode data 
+$info = $arr[result];      //select result array from JSON 
+$json = json_encode($info);    // encode the result array to json
+
+header("Access-Control-Allow-Origin: *"); header('Content-Type: application/json');
+echo $json;
+ // print the JSON
+
+}
+
