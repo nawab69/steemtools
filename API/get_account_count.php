6b0bab6eb405f3d8e1ed885c4dde06f0fea10298
Nawab69 <44573643+nawab69@users.noreply.github.com>
2019-05-18 13:25:56 +0600

    Update get_account_count.php

--- API/get_account_count.php
+++ API/get_account_count.php
@@ -4,3 +4,9 @@ $array = array('jsonrpc' => '2.0','method' => 'condenser_api.get_account_count',
     // create array
 $post = json_encode($array, JSON_PRETTY_PRINT);    // encode php array to JSON
 GetData($api,$post);    // Get Data function
+$total = array('total_account' => $info);
+$json = json_encode($total, JSON_PRETTY_PRINT);    // encode php array to JSON
+
+header("Access-Control-Allow-Origin: *"); header('Content-Type: application/json');
+echo $json;
+ // print the JSON
